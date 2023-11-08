2411a13dacd954557a9468ad9e885de98002f7ce
Jonathan Henrique <jonathanbispo085@gmail.com>
2023-11-08 16:35:59 -0300

    adding new health bar design + system, background and tanjiro spryte sheets

--- src/js/game.js
+++ src/js/game.js
@@ -1,99 +1,82 @@
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
 context.fillRect(0,0,canvas.width,canvas.height)
 
 const gravity = 0.5
 
-class Sprite {
-    constructor({position,velocity,color,offset}){
-        this.position = position
-        this.velocity = velocity
-        this.height = 150
-        this.width = 50
-        this.lastKey
-        this.movSpeed = 6
-        this.jumps = 2
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50
-        }
-        this.color = color
-        this.isAttacking
-        this.health = 100
-    }
-
-    draw() {
-        context.fillStyle = this.color
-        context.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-        // Attack box
-        if(this.isAttacking){
-            context.fillStyle = 'green'
-            context.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-        }
-    }
-
-    update() {
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        //Collision with ground
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-            this.jumps = 2
-        } else {
-            this.velocity.y += gravity
-        }
-
-        //Collision with walls
-        if(this.position.x <= 0) {
-            this.position.x = 0
-        } else if (this.position.x + this.width >= canvas.width){
-            this.position.x = canvas.width - this.width
-        }
-    }
-
-    attack() {
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100);
-    }
-}
-
-const player1 = new Sprite({
-    position:{
-        x: 70,
+const background = new SpriteScenery({
+    position: {
+        x: 0,
         y: 0
+    },
+    imageSrc: '/src/imagens/game-assets/scenery1.jpg'
+}, window.innerWidth, window.innerHeight)
+
+
+
+
+const player1 = new Character({
+    position: {
+        x: 0,
+        y: 400
     },
     velocity: {
         x: 0,
         y: 0
     },
-    color: 'red',
+    imageSrc: '/src/imagens/game-assets/tanjiro/tanjiro-idle.png',
+    scale: 2,
     offset: {
-        x: 0,
+        x: 215,
         y: 0
+    },
+    sprites: {
+        idle: {
+            imageSrc: '/src/imagens/game-assets/tanjiro/tanjiro-idle.png',
+            framesMax: 8,
+            framesHold: 10
+        },
+        idleInverted: {
+            imageSrc: '/src/imagens/game-assets/tanjiro/tanjiro-idle-inverted.png',
+            framesMax: 8,
+            framesHold: 10
+        },
+        run: {
+            imageSrc: '/src/imagens/game-assets/tanjiro/tanjiro-run.png',
+            framesMax: 8,
+            framesHold: 10
+        },
+        runInverted: {
+            imageSrc: '/src/imagens/game-assets/tanjiro/tanjiro-run-inverted.png',
+            framesMax: 8,
+            framesHold: 10
+        },
+        jump: {
+            imageSrc: '/src/imagens/game-assets/tanjiro/tanjiro-jump.png',
+            framesMax: 2,
+            framesHold: 10
+        },
+        jumpInverted: {
+            imageSrc: '/src/imagens/game-assets/tanjiro/tanjiro-jump-inverted.png',
+            framesMax: 2,
+            framesHold: 10
+        },
+        attack: {
+            imageSrc: '/src/imagens/game-assets/tanjiro/tanjiro-attack.png',
+            framesMax: 5,
+            framesHold: 5
+        }
     }
 })
 
 
-const player2 = new Sprite({
+const player2 = new Character({
     position:{
-        x: 900,
+        x: window.innerWidth - 70,
         y: 0
     },
     velocity: {
@@ -122,30 +105,49 @@ const keys = {
     }
 }
 
-function rectangularCollision ({rectangle1, rectangle2}) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
-        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle1.width && 
-        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && 
-        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-    )
+decreaseTimer()
+
+function isOnGround(player){
+    return player.position.y + player.height + player.velocity.y >= canvas.height - 70
 }
+
 
 function animate() {
     window.requestAnimationFrame(animate)
     context.fillStyle = 'black'
     context.fillRect(0,0,canvas.width,canvas.height)
+    background.update()
     player1.update()
-    player2.update()
+    // player2.update()
 
     player1.velocity.x = 0
     player2.velocity.x = 0
 
+    
     //Player 1 movement
     if(keys.a.pressed && player1.lastKey === 'a'){
         player1.velocity.x = -player1.movSpeed
+        player1.direction = -1
+        if(isOnGround(player1)){ player1.switchSprite('run-inverted') }
+
     } else if (keys.d.pressed && player1.lastKey === 'd'){
         player1.velocity.x = player1.movSpeed
+        player1.direction = 1
+        if(isOnGround(player1)){ player1.switchSprite('run') }
+    } else if (isOnGround(player1)){
+        if(player1.direction === -1){
+            player1.switchSprite('idle-inverted')
+        } else {
+            player1.switchSprite('idle')
+        }
+    }
+
+    if(player1.velocity.y < 0 || player1.velocity.y > 0) {
+        if(player1.direction === -1){
+            player1.switchSprite('jump-inverted')
+        } else {
+            player1.switchSprite('jump')
+        }
     }
 
     //Player 2 movement
@@ -166,7 +168,10 @@ function animate() {
             player1.isAttacking = false
             player2.color = 'yellow'
             player2.health -= 4 
-            document.querySelector('#player2Health').style.width = player2.health + '%'
+            document.querySelector('.health-quantity-P2').style.width = player2.health + '%'
+            setTimeout(() => {
+                document.querySelector('.lose-health-P2').style.width = player2.health + '%'
+            }, 1000);
     } else {
         player2.color = 'blue'
     }
@@ -180,7 +185,10 @@ function animate() {
             player2.isAttacking = false 
             player1.color = 'yellow'
             player1.health -= 4 
-            document.querySelector('#player1Health').style.width = player1.health + '%'
+            document.querySelector('.health-quantity-P1').style.width = player1.health + '%'
+            setTimeout(() => {
+                document.querySelector('.lose-health-P1').style.width = player1.health + '%'
+            }, 1000);
     } else {
         player1.color = 'red'
     }
@@ -196,8 +204,6 @@ function animate() {
     } else {
         player2.attackBox.offset.x = -50
     }
-
-   
 }
 
 animate()
