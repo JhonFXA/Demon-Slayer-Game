8a3c1c82749da31eebad181d7c48096b28b6c3c2
Jonathan Henrique <jonathanbispo085@gmail.com>
2023-11-11 13:31:45 -0300

    added new menu interface

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+function startGame(){
+    window.location.href = "../html/character-select.html"
+}
+
+for (const button of document.querySelectorAll('.inactive')) {
+    button.addEventListener('click',() => {
+        window.alert('Esta funcionalidade ainda será implementada! | This functionality is still in development!')
+    })
+}
